9fcaa59ac95aafe98fda7b563d5347fcadb223ad
zhenxinma <2280480546@qq.com>
2024-02-02 00:51:32 +0800

    feat: 测试打包压缩，结果失败

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,19 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+
+
 module.exports = defineConfig({
-  transpileDependencies: true,
-  // 解决vue空白页面. 
-  publicPath: "./"
+    transpileDependencies: ['element-ui'],
+    // 解决vue空白页面.
+    publicPath: "./",
+    productionSourceMap: false,
+    chainWebpack(config) {
+        config.plugins.delete('prefetch');
+        config.plugin('compressionPlugin').use(
+            new CompressionWebpackPlugin({
+                test: /\.(js|css|html)$/,
+                threshold: 1024, // 超过1K就压缩
+            })
+        )
+    }
 })
